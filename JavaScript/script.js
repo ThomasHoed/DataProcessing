9adf9c52f525d786712ffe060afd7911429d80e3
ThomasHoed <Thomas_hoed@live.nl>
2018-11-22 15:29:36 +0100

    added week 4 link to root index

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -12,6 +12,7 @@ d3.select("body").append("title").text("Assigments");
 // paragraph
 d3.select("body").append('h4').text("Links to assigments:")
 d3.select("body").append('a').attr("href", "https://thomashoed.github.io/DataProcessing/Homework/Week3/index.html").text("Week 3")
+d3.select("body").append('a').attr("href", "https://thomashoed.github.io/DataProcessing/Homework/Week4/index.html").text("Week 4")
 
 // footer
 d3.select("html").append("footer").append("p").attr("class","small text-center text-muted").text("Thomas Hoedeman, 10318070")
