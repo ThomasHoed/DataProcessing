bc9916eaadedf5c2bbdbda94cf8723c1b0c72989
ThomasHoed <Thomas_hoed@live.nl>
2018-12-10 19:27:37 +0100

    first commit week 6

---
+++ Homework/Week6/Data/DoubleCSV2JSON.py
@@ -0,0 +1,74 @@
+# name: Thomas Hoedeman
+# student number: 10318070
+
+
+# import
+from tkinter.filedialog import askopenfilename
+import csv
+import json
+
+# get input paths
+inputPaths = ["hpi-data-2016.csv", "worldhappiness2016.csv"]
+selection = ["first", "second"]
+for idx, input in enumerate(inputPaths):
+    while not inputPaths[idx][-4:] == '.csv':
+        # print instruction
+        print(f"Please open the {selection[idx]}.csv file")
+
+        # input user interface
+        inputPaths[idx]= askopenfilename()
+
+# load dataSet
+dataSets = []
+for path in inputPaths:
+    print(path)
+    # open csv file
+    with open(path, "r") as csvfile:
+
+        # read data
+        currentData = csv.reader(csvfile)
+
+
+
+        dataSets.append([line for line in currentData])
+
+# remove empty cells
+for idx, data in enumerate(dataSets):
+    for row_idx, row in enumerate(data):
+        if row[0] == '':
+            data_end = row_idx
+            break
+    dataSets[idx] = dataSets[idx][0:row_idx]
+
+# select if countries are present in both datasets
+countries = dict()
+for data in dataSets:
+    for row in data[1:]:
+        countries[row[0]] = 1
+for data in dataSets:
+    for row in data[1:]:
+        countries[row[0]] += 1
+
+selected_countries = [country for country in countries if countries[country] == 3]
+deleted = [country for country in countries if countries[country] != 3]
+
+outputJSON = dict()
+for country_name in selected_countries:
+    outputJSON[country_name] = dict()
+
+for data in dataSets:
+    headers =  data[0]
+    for row in data[1:]:
+        current_country = row[0]
+        if current_country in selected_countries:
+            for idx, column in enumerate(row):
+                if headers[idx] != "Country" and "Region":
+                    outputJSON[current_country][headers[idx]] = column
+
+# dump into .JSON file with the same name as .csv file
+with open(f'Combination_{inputPaths[0][:-4]}_&_{inputPaths[1][:-4]}.json', 'w') as f:
+    json.dump(outputJSON, f,indent=4)
+
+
+
+print("end")
---
+++ Homework/Week6/JavaScript/script.js
@@ -0,0 +1,10 @@
+/*
+name: Thomas Hoedeman
+student number: 10318070
+source World Hapiness Report: https://www.kaggle.com/unsdsn/world-happiness#2016.csv
+source Happy Planet Index: happyplanetindex.org/s/hpi-data-2016.xlsx
+Data edited for the purpose of this assigment
+*/
+window.onload = function(){
+    
+}
