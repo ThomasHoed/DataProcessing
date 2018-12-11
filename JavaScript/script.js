ff50a89d154b4567378f02ecf323ee3c003441ee
ThomasHoed <Thomas_hoed@live.nl>
2018-12-11 01:55:53 +0100

    added week 6 to index

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -13,6 +13,7 @@ d3.select("body").append('h4').text("Links to assigments:");
 d3.select("body").append('a').attr("href", "https://thomashoed.github.io/DataProcessing/Homework/Week3/index.html").text("Week 3");
 d3.select("body").append('a').attr("href", "https://thomashoed.github.io/DataProcessing/Homework/Week4/index.html").html("<br>Week 4");
 d3.select("body").append('a').attr("href", "https://thomashoed.github.io/DataProcessing/Homework/Week5/index.html").html("<br>Week 5");
+d3.select("body").append('a').attr("href", "https://thomashoed.github.io/DataProcessing/Homework/Week6/index.html").html("<br>Week 6");
 
 // footer
 d3.select("html").append("footer").append("p").attr("class","small text-center text-muted").text("Thomas Hoedeman, 10318070");
