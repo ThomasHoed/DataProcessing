a65cbc95b509ab748db4a117d66c8df7b66f94b6
ThomasHoed <Thomas_hoed@live.nl>
2018-12-13 13:43:05 +0100

    partial interactive

--- Homework/Week6/JavaScript/script.js
+++ Homework/Week6/JavaScript/script.js
@@ -23,8 +23,8 @@ window.onload = function(){
     })
 
     function getScatterData(data, factor1, factor2){
-        var countries = Object.keys(data);
 
+        var countries = Object.keys(data);
         var scatterData = [];
         countries.forEach(function(country){
             let array = [];
@@ -33,24 +33,123 @@ window.onload = function(){
             array.push(country);
             array.push(data[country]["Population"].replace(/,/g, ''));
             array.push(data[country]["Region"]);
-
-            delete array;
+            array.push(factor1);
+            array.push(factor2);
 
             scatterData.push(array)
+            delete array;
         });
         return scatterData
     }
 
+    function getPieData(data){
+
+        output = {};
+        var names = ["Economy", "Family", "Life Expectancy", "Freedom", "Government Corruption", "Generosity", "Dystopia Residual","Happiness Score", "Happiness Rank"];
+        var values = [];
+        for(i = 0; i < names.length; i++){
+
+            values.push(data[names[i]]);
+        }
+        output["values"] = values
+        output["names"] = names
+
+        return output
+    }
+
+    function makePieChart(data, country){
+
+        var country_data = data[country];
+        //  source: https://bl.ocks.org/adamjanes/53eedf0b915fd8b20f04fd08bc24ff00  & https://github.com/kthotav/D3Visualizations/blob/master/Pie_Charts/js/pie.js
+
+        var margin =  {top: 50, bottom: 50, left: 50, right: 50 }
+        var width = 560,
+            height = 280,
+            radius = Math.min(width, height) / 2;
+
+        var color = d3.scaleOrdinal()
+            .range(['#b35806','#f1a340','#fee0b6','#d8daeb','#998ec3','#542788']);
+
+        var pie = d3.pie()
+            .value(function(d) { return d; })
+            .sort(null);
+
+
+        var arc = d3.arc()
+            .innerRadius(radius - 50)
+            .outerRadius(radius - 10);
+
+        var labelArc = d3.arc()
+            .outerRadius(radius - 40)
+            .innerRadius(radius - 40);
+
+
+        var svgPie = d3.select("#piechart").append("svg")
+            .attr("width", width +  margin.left + margin.right)
+            .attr("height", height +  margin.top + margin.bottom )
+            .append("g")
+            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+        var all_data = getPieData(country_data)
+
+        var piedata =  all_data.values.slice(0,6)
+        var pieLabels =  all_data.names.slice(0,6)
+
+        var path = svgPie.datum(piedata).selectAll("path")
+                .data(pie)
+                .attr("class", "pie")
+                .enter().append("path")
+                .style("stroke", "black")
+                .attr("fill", function(d, i) { return color(i); })
+                .attr("d", arc)
+                .each(function(d) { this._current = d; })
+                .on("mouseover", function(d, i){
+                    svgPie.selectAll('text').html(function(){
+                        return  all_data.names[i] + ': ' + Math.round(all_data.values[i]*100)/100;
+                        });
+                })
+                .on("mouseout", function(d, i){svgPie.selectAll('text').html(country)});
+
+        svgPie.append("text")
+            .attr("transform", function(d) {
+            var _d = arc.centroid(d);
+            _d[0] *= 1.5;	//multiply by a constant factor
+            _d[1] *= 1.5;	//multiply by a constant factor
+            return "translate(" + _d + ")";
+            })
+            .attr("dy", ".50em")
+            .attr("stroke-width", 15)
+            .style("text-anchor", "middle")
+            .html(country);
+    }
+
+    function updatePie(data, country){
+        var country_data = data[country];
+        var all_data = getPieData(country_data)
+        var piedata =  all_data.values.slice(0,6)
+        var pieLabels =  all_data.names.slice(0,6)
+        console.log(pie);
+        var pie = d3.pie()
+            .value(function(d) { return d; })
+            .sort(null);
+
+        d3.selectAll('#piechart').selectAll('g').datum(piedata).selectAll("path").data(pie);
+        log
+
+    }
+
     function makeScatterPlot(input_data){
+        country = "Netherlands"
+        makePieChart(input_data, country)
 
-        factor1 = "Happy Planet Index"
-        factor2 = "Happiness Score"
+        factor1 = "Happiness Score"
+        factor2 = "Happy Planet Index"
         var data = getScatterData(input_data, factor1, factor2)
-
         var margin = getMargins()
 
-        var svg = d3.select("body")
+        var svg = d3.select("#scatterplot")
             .append("svg")
+            .attr("class", "scatterPlot")
             .attr("width", margin.w + margin.left + margin.right)
             .attr("height", margin.h + margin.top + margin.bottom)
             .append("g")
@@ -61,17 +160,28 @@ window.onload = function(){
         yScale = scales[1];
         popScale = scales[2];
 
-        plotRegressionLine(data, svg, xScale, yScale);
+        coords = regressionCoords(data, svg, xScale, yScale);
 
-        drawDots(data, svg, xScale, yScale, popScale);
+        svg.append('line')
+            .attr('id', 'regressionline')
+            .attr('x1', function(d) { return xScale(coords[0][0]); })
+            .attr('y1', function(d) { return yScale(coords[1][0]); })
+            .attr('x2', function(d) { return xScale(coords[0][1]); })
+            .attr('y2', function(d) { return yScale(coords[1][1]); })
+            .attr('stroke', 'red')
+            .style("stroke", "red")
+            .style('opacity', .90)
+            .attr('stroke-width', 5)
 
-        makeAxis(xScale, yScale, margin, svg);
+        drawDots(data, svg, xScale, yScale, popScale, input_data, factor1, factor2);
+
+        makeAxis(xScale, yScale, margin, svg, data[1]);
 
         legendOrdinal = makeLegend(data, svg, margin);
         svg.select(".legendOrdinal")
         .call(legendOrdinal);
 
-        title = "Are happy people making the planet happy?"
+        title = "How does happiness relate to socio-economic factors?"
         addTitle(svg, margin, title)
 
         // headers = getHeaders(input_data)
@@ -91,12 +201,14 @@ window.onload = function(){
             popData.push(country[3]);
         });
         // scales
+        console.log(xData);
+
         var yScale = d3.scaleLinear()
-            .range([margin.h, 5])
-            .domain([Math.round(Math.min.apply(Math, yData)* 0.95), Math.round(Math.max.apply(Math, yData) * 1.05)]);
+            .range([margin.h - margin.bottom, margin.top])
+            .domain([Math.min.apply(Math, yData)* 0.95, Math.max.apply(Math, yData) * 1.05]);
 
         var xScale = d3.scaleLinear()
-            .range([0, margin.w])
+            .range([margin.left, margin.w -  margin.right])
             .domain([Math.min.apply(Math, xData) * 0.95, Math.max.apply(Math, xData) * 1.05]);
 
         var popScale = d3.scaleLinear()
@@ -127,17 +239,17 @@ window.onload = function(){
     }
 
     function getMargins(){
-        var margins = {top: 50, right: 500, bottom: 50, left: 100};
-        margins.w = 1100 - margins.left - margins.right;
-        margins.h = 650 - margins.top - margins.bottom;
+        var margins = {top: 40, right: 150, bottom: 40, left: 60};
+        margins.w = 900;
+        margins.h = 590;
+        margins.r = Math.min(margins.w, margins.h) / 2;
         return margins
     }
 
-    function drawDots(data, svg, xScale, yScale, popScale){
+    function drawDots(data, svg, xScale, yScale, popScale, input_data){
         //  source colors: http://colorbrewer2.org/#type=diverging&scheme=BrBG&n=10
         var colorMapping = makeColorMapping(data);
-
-        tip = makeTip();
+        tip = makeTip(factor1, factor2);
         svg.call(tip)
 
         svg.selectAll("circle")
@@ -159,50 +271,56 @@ window.onload = function(){
                  return colorMapping[d[4]]
              })
              .on('mouseover', tip.show)
-             .on('mouseout', tip.hide);
+             .on('mouseout', tip.hide)
+             .on('click', function(d){
+                 updatePie(input_data, d[2])
+             });
     }
 
-    function makeTip(){
+    function makeTip(title){
         // source: http://bl.ocks.org/Caged/6476579
         var tip = d3.tip()
             .attr('class', 'd3-tip')
+            .attr('id', "tooltip")
             .offset([-10, 0])
-            .html(function(d) {return "<span style='color:white'>" + "Country: " + d[2] + "<br>Population: " + Math.round(d[3]/100000)/10 + " Million"+ "<br>HPI: "+ d[0]+ " ||  Hapiness: " + Math.round(d[1]*100)/100 + " </span>";});
+            .html(function(d) {
+                return "<span style='color:white'>" + "Country: " + d[2] + "<br>Population: " + Math.round(d[3]/100000)/10 + " Million"+ "<br>"+ d[5]+ ": " + d[0]+ " || " + d[6] + ": " + Math.round(d[1]*100)/100 + " </span>";});
 
         return tip
     }
 
-    function makeAxis(xScale, yScale, margin, svg){
+    function makeAxis(xScale, yScale, margin, svg, data){
         // y
         svg.append("g")
-            .attr("class", "xAxis")
-            .attr("transform", "translate(-5, 0)")
+            .attr("class", "yAxis")
+            .attr("transform", "translate("+(margin.left -5)+", 0)")
             .attr("stroke-width", 2)
-            .attr("y", 0)
             .call(d3.axisLeft(yScale))
             .attr("font-family", "sans-serif").attr("font-size", "12px");
 
         // ylabel
         svg.append("text")
-            .text("Happiness score")
-            .attr("y", -margin.left/2)
-            .style("text-anchor", "end")
-            .attr("transform", "rotate(-90)");
+            .attr("class", "yLabel")
+            .text(data[6])
+            .attr("y", margin.top - 10)
+            .attr("x", margin.left)
+            .style("text-anchor", "middle")
+            // .attr("transform", "rotate(-90)");
 
         // x
         svg.append("g")
-            .attr("class", "yAxis")
+            .attr("class", "xAxis")
             .attr("stroke-width", 2)
-            .attr("transform", "translate(0," +  (margin.h +5)+ ")")
+            .attr("transform", "translate("+ 0 + "," + (margin.h - margin.bottom) + ")")
             .call(d3.axisBottom(xScale))
             .attr("font-family", "sans-serif").attr("font-size", "12px").attr("text-anchor", "middle");
 
         // xlabel
         svg.append("text")
-            .text("Happy Planet Index")
-            .attr("y", margin.h + margin.bottom -  margin.bottom / 4)
-            .attr("x", margin.w / 2 )
-            .style("text-anchor", "middle");
+            .text(data[5])
+            .attr("y", margin.h)
+            .attr("x", (margin.w - margin.left - margin.right)/2)
+            .style("text-anchor", "start");
 
     }
 
@@ -220,7 +338,7 @@ window.onload = function(){
         svg.append("g")
         .attr("class", "legendOrdinal")
         .attr("font-size", "12px")
-        .attr("transform", "translate("+ margin.w +",20)");
+        .attr("transform", "translate("+ (margin.w -  margin.right) +","+ margin.top +")");
 
 
         var legendOrdinal = d3.legendColor()
@@ -246,7 +364,7 @@ window.onload = function(){
 
     }
 
-    function plotRegressionLine(data, svg, xScale, yScale){
+    function regressionCoords(data, svg, xScale, yScale){
         // // Get data in arrays
         let values_x = [];
         let values_y = [];
@@ -313,28 +431,27 @@ window.onload = function(){
             result_values_y.push(y);
         }
 
-        // plot line
-        svg.append('line')
-             .attr('id', 'trendline')
-             .attr('x1', function(d) { return xScale(Math.min.apply(Math, result_values_x)); })
-             .attr('y1', function(d) { return yScale(Math.min.apply(Math, result_values_y)); })
-             .attr('x2', function(d) { return xScale(Math.max.apply(Math, result_values_x)); })
-             .attr('y2', function(d) { return yScale(Math.max.apply(Math, result_values_y)); })
-             .attr('stroke', 'red')
-             .style('opacity', .55)
-             .attr('stroke-width', 4.5);
+        // console.log(result_values_x);
+        // console.log(result_values_y);
 
+        var x_min = Math.min.apply(Math, result_values_x)
+        var x_max = Math.max.apply(Math, result_values_x)
+        var x_min_index = result_values_x.indexOf(x_min)
+        var x_max_index = result_values_x.indexOf(x_max)
+
+        var x = [x_min, x_max];
+        var y = [result_values_y[x_min_index], result_values_y[x_max_index]];
+
+        return [x, y]
     }
 
     function makeMenu(data, svg){
         headers =  Object.keys(data["Netherlands"]);
         for(i = headers.length - 1; i >= 0; i--){
-            if (headers[i] == "Happy Planet Index" || headers[i] == "HPI Index" || headers[i] == "HPI Rank" || headers[i] == "Region"){
+            if (headers[i] == "Happiness Rank" || headers[i] == "Happiness Score" || headers[i] == "Region" || headers[i] == "GDP"){
                 headers.splice(i, 1);
             }
         }
-
-        console.log(headers);
         d3.select('#x-axis-menu')
         .selectAll('li')
         .data(headers)
@@ -342,7 +459,7 @@ window.onload = function(){
         .append('li')
         .text(function(d) {return d;})
         .on('click', function(d) {
-            factor1 = "Happy Planet Index"
+            factor1 = "Happiness Score"
             factor2 = d;
             new_data = getScatterData(data, factor1, factor2);
             updateScatter(new_data, factor2, svg)
@@ -350,35 +467,43 @@ window.onload = function(){
     }
 
     function updateScatter(dat, title, svg){
-        scales = makeScales(dat);
-        xScale = scales[0];
-        yScale = scales[1];
-        popScales = scales[2]
-        margin = getMargins();
-        console.log(dat[0]);
+        let scales = makeScales(dat);
+        let xScale = scales[0];
+        let yScale = scales[1];
+        let popScales = scales[2]
+        let margin = getMargins();
 
-
-
-        d3.selectAll("yAxis")
+        d3.selectAll(".yLabel")
             .transition()
             .duration(1500)
-            .attr("transform", "translate(0," + margin.h + ")")
-            .call(d3.axisLeft(yScale))
-            .attr("font-family", "sans-serif").attr("font-size", "10px").attr("text-anchor", "middle");
+            .text(title)
+
+        d3.selectAll(".yAxis")
+            .transition()
+            .duration(1500)
+            .call(d3.axisLeft(yScale));
+
+        let coords = regressionCoords(dat, svg, xScale, yScale)
+
+        svg.selectAll("#regressionline")
+            .transition()
+            .duration(1500)
+            .attr('x1', function(d) { return xScale(coords[0][0]); })
+            .attr('y1', function(d) {
+                if(yScale(coords[1][0]) > yScale(0)){
+                    return yScale(0)
+                }
+                return yScale(coords[1][0]); })
+            .attr('x2', function(d) { return xScale(coords[0][1]); })
+            .attr('y2', function(d) { return yScale(coords[1][1]); });
 
         // new dots
         d3.selectAll('circle')
+          .data(dat)
           .transition()
           .duration(1500)
           .attr("cx", function(d, i) {return xScale(dat[i][0]);})
-          .attr("cy", function(d, i) {return yScale(dat[i][1]);})
-          .attr("r", function(d, i)  {
-              let size = popScale(dat[i][3])
-              if(size > 18){
-                  size = 18
-              }
-              return size
-          })
+          .attr("cy", function(d, i) {return yScale(dat[i][1]);});
 
     }
 
